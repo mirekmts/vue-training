02f866ce29cb0bd29568dcc6839a153a7ad69c29
Mirosław Stasiak <mirek.stasiak@pronos.eu>
2018-09-15 11:02:33 +0200

    try v-for, add/remove element

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-const app =new Vue({
+const app = new Vue({
     el: '#app',
     data: {
-        greeting: 'Hello DevMeetings',
+        title: 'What you need to buy',
+        newItem: '',
+        list: [{
+            id: 0,
+            name: 'Milk',
+          }, {
+            id: 1,
+            name: 'Potato',
+        }]
+    },
+    methods: {
+        addItem() {
+            const text = this.newItem.trim();
+
+            if (text) {
+                this.list.push({
+                    id: this.list.length,
+                    name: text,
+                });
+                this.newItem = '';
+            }
+        },
+        removeItem(id) {
+            this.list.splice(id, 1);
+        }
     }
 })
